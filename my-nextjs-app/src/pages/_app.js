d9ef448c7e9f0d66e418ab1c32fc2cd39fa5d433
Bill Huang <billonline33@hotmail.com>
2024-03-03 11:26:25 +1100

    Add basic header

--- my-nextjs-app/src/pages/_app.js
+++ my-nextjs-app/src/pages/_app.js
@@ -1,5 +1,16 @@
 import "@/styles/globals.css";
+import styles from "@/styles/App.module.css";
+import Header from "@/components/Header/Header";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={styles.container}>
+      <Header />
+      <Component {...pageProps} />
+    </div>
+  );
 }
